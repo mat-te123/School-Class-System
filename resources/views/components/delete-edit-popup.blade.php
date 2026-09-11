ad060999789d6fd7c96766028079ba56a6d6cc49
mat-te123 <denodelviano@gmail.com>
2026-09-11 10:33:43 +0700

    refactor(admin): change delete-edit-modal logic to reuseable component

--- resources/views/components/delete-edit-popup.blade.php
+++ resources/views/components/delete-edit-popup.blade.php
@@ -1,30 +1,29 @@
 @props([
+    'deleteUrl' => '',
+    'updateUrl' => '',
+    'data' => null,
     'item' => null,
     'confirmationData' => '',
 ])
 {{-- test URL --}}
-<div x-show="showeditdeletepopup" x-clock x-transition.opacity @click.outside="showeditdeletepopup = false"
+<div x-show="showeditdeletepopup" x-cloak x-transition.opacity @click.outside="showeditdeletepopup = false"
     class="bg-white p-2 flex flex-col gap-2 w-full max-w-16.75 h-fit rounded-lg border border-gray-200 shadow-lg">
-    <span class="w-full text-red-500 bg-red-50 py-0.5 px-3 rounded-md hover:bg-red-100 cursor-pointer"
+    <span class="text-sm w-full text-red-500 bg-red-50 py-0.5 px-3 rounded-md hover:bg-red-100 cursor-pointer"
         @click="
         showDeleteModal= true;
         showeditdeletepopup= false;
         confirmationData='{{ addslashes($confirmationData) }}';
-        deleteUrl='{{ route('periode-penjurusan.destroy', ['id' => $item->id]) }}';  
+        deleteUrl='{{ $deleteUrl }}';  
          ">
         delete
     </span>
-    <span class="w-full text-blue-500 bg-blue-50 py-0.5 px-3 text-center rounded-md hover:bg-blue-100 cursor-pointer"
+    <span
+        class="text-sm w-full text-blue-500 bg-blue-50 py-0.5 px-3 text-center rounded-md hover:bg-blue-100 cursor-pointer"
         @click="
         showupdateModal= true;
         showeditdeletepopup= false;
-        periodeData={ 
-            ...{{ json_encode($item) }},
-            is_active: {{ $item->is_active ? '1' : '0' }}, 
-            tanggal_buka: '{{ $item->tanggal_buka }}'.split(' ')[0],
-            tanggal_tutup: '{{ $item->tanggal_tutup }}'.split(' ')[0],
-        };
-        editUrl='{{ route('periode-penjurusan.update', ['id' => $item->id]) }}';
+        Data=@js($data);
+        updateUrl='{{ $updateUrl }}';
 
     ">
         edit
