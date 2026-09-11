93895ad150b46bcd6e9737b396d4b26721e483f8
mat-te123 <denodelviano@gmail.com>
2026-09-11 10:35:00 +0700

    docs: add new link

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -2,8 +2,9 @@
     <div class="text-xl font-bold px-2">School App</div>
 
     <nav class="flex flex-col gap-2">
-        <a href="{{ route('admin-siswa.index') }}" class="p-2 rounded hover:bg-slate-800">Dashboard</a>
+        <a href="{{ route('admin-siswa.index') }}" class="p-2 rounded hover:bg-slate-800">Siswa</a>
         <a href="{{ route('admin-periode-penjurusan.index') }}" class="p-2 rounded hover:bg-slate-800">Periode</a>
+        <a href="{{ route('admin-paket-menu.index') }}" class="p-2 rounded hover:bg-slate-800">Kelas</a>
         <a href="#" class="p-2 rounded hover:bg-slate-800">Nilai</a>
     </nav>
 
